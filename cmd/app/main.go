778602eef2d32ab2a17ba01d7bf745e5c23bab4b
Denis <ledorubden@gmail.com>
2024-04-16 01:29:51 +0300

    Add server startup.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/ledorub/snote-api/api/router"
 	"github.com/ledorub/snote-api/internal/config"
 	"github.com/ledorub/snote-api/internal/logger"
+	"net/http"
 )
 
 func main() {
@@ -11,4 +13,13 @@ func main() {
 	fmt.Printf("Got port %d.\n", cfg.Port)
 	log := logger.New()
 	log.Println("Set up logger.")
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.Port),
+		Handler: router.New(log),
+	}
+
+	log.Printf("Starting the server at %s", srv.Addr)
+	err := srv.ListenAndServe()
+	log.Fatal(err)
 }
