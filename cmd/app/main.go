82866a5c190b3d3673c3c30541d26216241f8126
Denis <ledorubden@gmail.com>
2024-04-16 00:21:58 +0300

    Add stdout logger.

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"github.com/ledorub/snote-api/internal/config"
+	"github.com/ledorub/snote-api/internal/logger"
 )
 
 func main() {
 	cfg := config.New()
-	fmt.Printf("Got port %d.", cfg.Port)
+	fmt.Printf("Got port %d.\n", cfg.Port)
+	log := logger.New()
+	log.Println("Set up logger.")
 }
