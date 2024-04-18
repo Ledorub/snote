b940491c08a95c33497eec7be9ded09bc54898ef
Denis <ledorubden@gmail.com>
2024-04-18 23:27:31 +0300

    Update API handlers.

--- api/resource/note/handler.go
+++ api/resource/note/handler.go
@@ -4,22 +4,33 @@ import (
 	"github.com/ledorub/snote-api/api/common"
 	"log"
 	"net/http"
+	"time"
 )
 
 type API struct {
 	logger         *log.Logger
+	requestReader  common.RequestReader
 	responseWriter common.ResponseWriter
 }
 
-func New(logger *log.Logger, responseWriter common.ResponseWriter) *API {
-	return &API{logger: logger, responseWriter: responseWriter}
+func New(logger *log.Logger, requestReader common.RequestReader, responseWriter common.ResponseWriter) *API {
+	return &API{logger: logger, requestReader: requestReader, responseWriter: responseWriter}
 }
 
 func (api *API) Create(w http.ResponseWriter, r *http.Request) {
-	note := map[string]any{
-		"id":      2,
-		"content": "encrypted content",
+	type noteCreate struct {
+		Content           string        `json:"content"`
+		ExpiresAt         time.Time     `json:"expiresAt"`
+		ExpiresAtTimezone string        `json:"expiresAtTimezone"`
+		ExpiresIn         time.Duration `json:"expiresIn"`
+		KeyHash           string        `json:"keyHash"`
 	}
+	note := noteCreate{}
+	if err := api.requestReader.Read(r.Body, &note); err != nil {
+		api.responseWriter.WriteBadRequest(w, r, err)
+		return
+	}
+
 	api.responseWriter.Write(w, r, http.StatusCreated, note)
 }
 
