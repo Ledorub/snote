caac6117ce2805c8b2a1df2f6c8f133de6822ea8
Denis <ledorubden@gmail.com>
2024-04-18 23:18:40 +0300

    Implement Decode method on JSONDecoder.

--- internal/encdec/json.go
+++ internal/encdec/json.go
@@ -2,12 +2,15 @@ package encdec
 
 import (
 	gojson "encoding/json"
+	"errors"
+	"fmt"
 	"io"
+	"strings"
 )
 
 type JSONEncoder struct{}
 
-func (enc *JSONEncoder) Encode(data map[string]any) ([]byte, error) {
+func (enc *JSONEncoder) Encode(data any) ([]byte, error) {
 	return gojson.Marshal(data)
 }
 
@@ -18,6 +21,42 @@ func NewJSONEncoder() *JSONEncoder {
 type JSONDecoder struct{}
 
 func (enc *JSONDecoder) Decode(data io.Reader, dst any) error {
+	dec := gojson.NewDecoder(data)
+	dec.DisallowUnknownFields()
+
+	if err := dec.Decode(dst); err != nil {
+		var (
+			syntaxError            *gojson.SyntaxError
+			unmarshalTypeError     *gojson.UnmarshalTypeError
+			invalidUnmarshallError *gojson.InvalidUnmarshalError
+		)
+
+		switch {
+		case errors.As(err, &syntaxError):
+			return fmt.Errorf("data contains badly-formed JSON (at character %d)", syntaxError.Offset)
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return fmt.Errorf("data contains incorrect JSON type for field %q", unmarshalTypeError.Field)
+			}
+			return fmt.Errorf("body contains incorrect JSON type (at character %d)", unmarshalTypeError.Offset)
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			// https://github.com/golang/go/issues/25956
+			return errors.New("data contains badly-formed JSON")
+		case errors.Is(err, io.EOF):
+			return errors.New("data must not be empty")
+		case errors.As(err, &invalidUnmarshallError):
+			panic(err)
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return fmt.Errorf("json contains unknown key %s", fieldName)
+		default:
+			return err
+		}
+	}
+
+	if err := dec.Decode(&struct{}{}); !errors.Is(err, io.EOF) {
+		return errors.New("body must only contain a single JSON value")
+	}
 	return nil
 }
 
