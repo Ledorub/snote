b1c19ed2533b37b6c492aa731e91920b0ec561fe
Denis <ledorubden@gmail.com>
2024-04-19 17:56:50 +0300

    Create Validator.

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -2,6 +2,35 @@ package validator
 
 import "cmp"
 
+type Validator struct {
+	NonFieldErrors []string
+	FieldErrors    map[string]string
+}
+
+func (v *Validator) CheckIsValid() bool {
+	return len(v.NonFieldErrors) == 0 && len(v.FieldErrors) == 0
+}
+
+func (v *Validator) AddNonFieldError(message string) {
+	v.NonFieldErrors = append(v.NonFieldErrors, message)
+}
+
+func (v *Validator) AddFieldError(field, message string) {
+	if v.FieldErrors == nil {
+		v.FieldErrors = make(map[string]string)
+	}
+
+	if _, exists := v.FieldErrors[field]; !exists {
+		v.FieldErrors[field] = message
+	}
+}
+
+func (v *Validator) CheckField(field string, ok bool, message string) {
+	if !ok {
+		v.AddFieldError(field, message)
+	}
+}
+
 func ValidateValueInRange[T cmp.Ordered](v, low, high T) bool {
 	return v >= low && v <= high
 }
