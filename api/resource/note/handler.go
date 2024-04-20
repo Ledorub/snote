a8672a909bcc5f25a5c91c4754ca4e1d87e3a7b0
Denis <ledorubden@gmail.com>
2024-04-20 20:59:17 +0300

    Update note.Create method on note.API to include input validation.

--- api/resource/note/handler.go
+++ api/resource/note/handler.go
@@ -25,11 +25,11 @@ func New(logger *log.Logger, requestReader common.RequestReader, responseWriter 
 
 func (api *API) Create(w http.ResponseWriter, r *http.Request) {
 	type noteCreate struct {
-		Content           string        `json:"content"`
-		ExpiresAt         time.Time     `json:"expiresAt"`
-		ExpiresAtTimezone string        `json:"expiresAtTimezone"`
-		ExpiresIn         time.Duration `json:"expiresIn"`
-		KeyHash           string        `json:"keyHash"`
+		Content           string    `json:"content"`
+		ExpiresAt         time.Time `json:"expiresAt"`
+		ExpiresAtTimezone string    `json:"expiresAtTimezone"`
+		ExpiresIn         int       `json:"expiresIn"`
+		KeyHash           string    `json:"keyHash"`
 	}
 	note := noteCreate{}
 	if err := api.requestReader.Read(r.Body, &note); err != nil {
@@ -37,6 +37,43 @@ func (api *API) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	v := api.validatorFactory()
+	v.CheckField("content", note.Content != "", "must not be empty")
+	v.CheckField("content", len(note.Content) <= 1024*1024, "must not be bigger than 1 MB")
+	v.CheckField("keyHash", len(note.KeyHash) == 64, "must be exactly 64 bytes long")
+
+	var t time.Time
+	isExpiresInSet := note.ExpiresIn != 0
+	isExpiresAtSet := note.ExpiresAt != t && note.ExpiresAtTimezone != ""
+	expirationDateConflict := isExpiresInSet && isExpiresAtSet
+	if expirationDateConflict || !(isExpiresInSet || isExpiresAtSet) {
+		v.AddNonFieldError("either expiresIn or both expiresAt and expiresAtTimezone should be provided")
+	} else {
+		if note.ExpiresIn == 0 {
+			v.CheckField("expiresAt", note.ExpiresAt == t, "should be provided")
+			_, offset := note.ExpiresAt.Zone()
+			v.CheckField("expiresAt", offset != 0, "should not include a time zone")
+			v.CheckField("expiresAtTimezone", note.ExpiresAtTimezone == "", "should be provided")
+
+			tz, err := time.LoadLocation(note.ExpiresAtTimezone)
+			v.CheckField("expiresAtTimezone", err != nil, "should be a valid time zone id")
+
+			if v.CheckIsValid() {
+				expirationTime := note.ExpiresAt.In(tz)
+				expiresAtGELimit := time.Until(expirationTime) >= 10*time.Minute
+				v.CheckField("expiresAt", expiresAtGELimit, "should be at least 10 minutes in the future")
+			}
+		} else {
+			expiresInGELimit := note.ExpiresIn*int(time.Second) >= int(10*time.Minute)
+			v.CheckField("expiresIn", expiresInGELimit, "should not be less than 600 seconds")
+		}
+	}
+
+	if !v.CheckIsValid() {
+		errors := common.MergeValidationErrors(v)
+		api.responseWriter.WriteValidationError(w, r, errors)
+		return
+	}
 	api.responseWriter.Write(w, r, http.StatusCreated, note)
 }
 
