3f308cb0861cca7150b7cb9b4cddc384ca024232
Denis <ledorubden@gmail.com>
2024-04-23 00:19:46 +0300

    Create DB startup script.

--- db.Dockerfile
+++ db.Dockerfile
@@ -2,5 +2,8 @@ FROM postgres:16
 
 WORKDIR /usr/local/bin
 
-COPY scripts/db_healthcheck.sh .
-RUN ["chmod", "+x", "db_healthcheck.sh"]
+COPY scripts/db_*.sh .
+RUN ["chmod", "+x", "db_startup.sh", "db_healthcheck.sh"]
+
+ENTRYPOINT ["db_startup.sh", "docker-entrypoint.sh"]
+CMD ["postgres"]
---
+++ scripts/db_startup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+source db_secret.sh
+
+NOT_FOUND_E=66
+NO_PERMISSION_E=77
+
+main () {
+  declare -A credentials
+
+  if [[ $# -eq 3 ]]; then
+    if ! check_file_exists $1; then
+      fatal_error $NOT_FOUND_E "File not found: $1"
+    fi
+    if ! check_has_read_permission $1; then
+      fatal_error $NO_PERMISSION_E "Permission error: $1"
+    fi
+
+    secret_path=$(dirname $1)
+    read_credentials_from_file $1 credentials secret_path
+    shift
+  fi
+
+  if ! check_file_exists $1; then
+    fatal_error $NOT_FOUND_E "File not found: $1"
+  fi
+  if ! check_has_read_permission $1; then
+    fatal_error $NO_PERMISSION_E "Permission error: $1"
+  fi
+
+  credentials_string=""
+  credentials_to_string credentials credentials_string
+
+  start_db $@
+}
+
+fatal_error() {
+  echo $2
+  exit $1
+}
+
+start_db() {
+  exec env $credentials_string "$@"
+}
+
+main "$@"
