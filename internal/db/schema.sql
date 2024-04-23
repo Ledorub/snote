d39e0ce1e7c33de223836b6a62f0161fec37fb4e
Denis <ledorubden@gmail.com>
2024-04-23 21:04:51 +0300

    Add length check to note.key_hash field.

--- internal/db/schema.sql
+++ internal/db/schema.sql
@@ -5,4 +5,6 @@ CREATE TABLE note (
     expires_at TIMESTAMP NOT NULL,
     expires_at_timezone TEXT NOT NULL,
     key_hash BYTEA NOT NULL
+
+    CONSTRAINT chk_key_hash_32_bytes CHECK (length(key_hash) = 32)
 );
