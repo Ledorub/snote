cbf9cea1352369534dc30861f74ade6c7cf5f2d3
Denis <ledorubden@gmail.com>
2024-04-23 20:34:39 +0300

    Update schema.sql by removing the default value for note.created_at.

--- internal/db/schema.sql
+++ internal/db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE note (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
     content TEXT NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     expires_at TIMESTAMP NOT NULL,
     expires_at_timezone TEXT NOT NULL
 );
