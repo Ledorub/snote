fff1dec0e235d790d8e58f069d57da9fecefd66e
Denis <ledorubden@gmail.com>
2024-05-04 23:15:12 +0300

    Refactor config structure.

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,12 +6,24 @@ import (
 	"log"
 )
 
+type source string
+
+const (
+	FileSource     source = "file"
+	ArgumentSource source = "argument"
+)
+
+type configValue[T any] struct {
+	Value  T
+	Source source
+}
+
 type Config struct {
 	Server ServerConfig
 }
 
 type ServerConfig struct {
-	Port int
+	Port configValue[uint64]
 }
 
 func New() *Config {
@@ -21,10 +33,11 @@ func New() *Config {
 func loadFromArgs() *Config {
 	var cfg Config
 
-	flag.IntVar(&cfg.Server.Port, "port", 4000, "API server port")
+	flag.Uint64Var(&cfg.Server.Port.Value, "port", 4000, "API server port")
+	cfg.Server.Port.Source = FileSource
 	flag.Parse()
 
-	if !validator.ValidateValueInRange[int](cfg.Server.Port, 1024, 65535) {
+	if !validator.ValidateValueInRange[uint64](cfg.Server.Port.Value, 1024, 65535) {
 		log.Fatalf("Invalid port value %d. Should be in-between 1024 and 65535", cfg.Server.Port)
 	}
 	return &cfg
