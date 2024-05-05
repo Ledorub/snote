1a82d1222f7095b87fdea93e1ed83090c57fd593
Denis <ledorubden@gmail.com>
2024-05-05 22:51:16 +0300

    Create config value setters for flags.

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,6 +9,7 @@ import (
 type source string
 
 const (
+	SourceUnset    source = ""
 	FileSource     source = "file"
 	ArgumentSource source = "argument"
 )
@@ -30,15 +31,43 @@ func New() *Config {
 	return loadFromArgs()
 }
 
+type argReg[T any] func(name string, value T, usage string) *T
+
+type valueSetter map[string]func()
+
+func (vs valueSetter) addSetterFor(name string, setter func()) {
+	vs[name] = setter
+}
+
+func (vs valueSetter) setValueFor(name string) {
+	if setter, exists := vs[name]; exists {
+		setter()
+	}
+}
+
+func addArg[T any](valueSetter *valueSetter, reg argReg[T], cfgValue *configValue[T], name string, value T, usage string) {
+	parsedValue := reg(name, value, usage)
+	valueSetter.addSetterFor(name, func() {
+		if cfgValue.Source == SourceUnset {
+			cfgValue.Value = *parsedValue
+			cfgValue.Source = ArgumentSource
+		}
+	})
+}
+
 func loadFromArgs() *Config {
 	var cfg Config
 
-	flag.Uint64Var(&cfg.Server.Port.Value, "port", 4000, "API server port")
-	cfg.Server.Port.Source = FileSource
+	setter := &valueSetter{}
+	addArg[uint64](setter, flag.Uint64, &cfg.Server.Port, "port", 4000, "API server port")
+
 	flag.Parse()
+	flag.Visit(func(f *flag.Flag) {
+		setter.setValueFor(f.Name)
+	})
 
 	if !validator.ValidateValueInRange[uint64](cfg.Server.Port.Value, 1024, 65535) {
-		log.Fatalf("Invalid port value %d. Should be in-between 1024 and 65535", cfg.Server.Port)
+		log.Fatalf("Invalid port value %d. Should be in-between 1024 and 65535", cfg.Server.Port.Value)
 	}
 	return &cfg
 }
