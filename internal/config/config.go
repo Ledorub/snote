da9ea8fd328c39fe18bac3cc618aaf0cfadc1421
Denis <ledorubden@gmail.com>
2024-05-09 22:39:40 +0300

    Create secretString type for sensitive data.

--- internal/config/config.go
+++ internal/config/config.go
@@ -222,3 +222,27 @@ type configFileServer struct {
 type configFile struct {
 	Server configFileServer
 }
+
+type secretString struct {
+	value string
+}
+
+func (s secretString) String() string {
+	return "***"
+}
+
+func (s secretString) GoString() string {
+	return s.String()
+}
+
+func (s secretString) GetValue() string {
+	return s.value
+}
+
+func (s secretString) MarshalYAML() (any, error) {
+	return s.String(), nil
+}
+
+func (s secretString) UnmarshalYAML(unmarshal func(any) error) error {
+	return unmarshal(&s.value)
+}
